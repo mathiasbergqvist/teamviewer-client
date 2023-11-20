75b6cb723d0aff72f15a28a018bb82072c99a8d0
Mathias <mathiasbergqvist88@gmail.com>
2023-11-20 17:56:50 +0100

    New component: Reveal

---
+++ src/routes/teamviewer/Reveal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {Button, P} from 'flowbite-svelte';
+
+    export let answer: string;
+    export let title: string;
+    export let revealed: boolean = false;
+
+    const handleReveal = () => {
+        revealed = true;
+    };
+</script>
+
+<div>
+    <P size="lg">
+        {title}:
+        {#if revealed}
+            {answer}
+        {:else}
+            <Button on:click={handleReveal}>???</Button>
+        {/if}
+    </P>
+</div>
+
+<style>
+    div {
+        margin: 15px 0;
+    }
+</style>
