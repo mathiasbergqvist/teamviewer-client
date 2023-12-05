8239cdb14a11af2f26e945e33960cb63fd1aada4
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2023-12-05 21:47:56 +0100

    Add dependency to country-flag-emoji

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-	import "../app.pcss";
-    import Header from './Header.svelte';
-    import './styles.css';
+	import '../app.pcss';
+	import Header from './Header.svelte';
+	import './styles.css';
 </script>
 
 <div class="app">
 	<Header></Header>
 
 	<main>
-		<slot></slot>
+		<slot />
 	</main>
 
 	<footer>
