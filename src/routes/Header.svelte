461b87dc5b5cd091a5e1be0ad659ae8eb4fffd02
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2024-08-01 15:12:27 +0200

    Add page logotype and new header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,42 +1,71 @@
 <script>
 	import { page } from '$app/stores';
+	import logo from '$lib/images/main-logo.jpeg';
 </script>
 
 <header>
-	<div class="corner"></div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Team viewer</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/add-player') ? 'page' : undefined}>
-				<a href="/add-player">Add Player</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/edit-player') ? 'page' : undefined}>
-				<a href="/edit-player">Edit Player</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/add-team') ? 'page' : undefined}>
-				<a href="/add-team">Add Team</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/edit-team') ? 'page' : undefined}>
-				<a href="/edit-team">Edit Team</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner"></div>
+	<div class="logo-container">
+		<img alt="Team Viewer main logo" src={logo} width="180" height="180" />
+		<p class="logo-text">Team Viewer</p>
+	</div>
+	<div class="nav-container">
+		<div class="corner"></div>
+		<nav>
+			<svg viewBox="0 0 2 3" aria-hidden="true">
+				<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+			</svg>
+			<ul>
+				<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">Team viewer</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/add-player') ? 'page' : undefined}>
+					<a href="/add-player">Add Player</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/edit-player') ? 'page' : undefined}>
+					<a href="/edit-player">Edit Player</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/add-team') ? 'page' : undefined}>
+					<a href="/add-team">Add Team</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/edit-team') ? 'page' : undefined}>
+					<a href="/edit-team">Edit Team</a>
+				</li>
+			</ul>
+			<svg viewBox="0 0 2 3" aria-hidden="true">
+				<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+			</svg>
+		</nav>
+		<div class="corner"></div>
+	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
+		flex-direction: column;
+	}
+
+	.logo-container {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		height: 200px;
+		gap: 50px;
+		background: var(--color-theme-1);
+		justify-content: center;
+	}
+
+	.logo-text {
+		font-family: var(--font-montserrat);
+		color: var(--color-bg-1);
+		text-shadow: black 1px 0 10px;
+		font-size: 2.5rem;
+		font-weight: bold;
+	}
+
+	.nav-container {
+		display: flex;
+		flex-direction: row;
 		justify-content: space-between;
 	}
 
